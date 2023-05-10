88813ff99b24135e4eddf70581e46cf1f4d6eb15
thaizerbinatti <thatazerb@gmail.com>
2023-05-09 21:44:30 -0300

    aula 9maio

---
+++ Atividades Moodle/AtividadeJS03/main.js
@@ -0,0 +1,18 @@
+//Quantidade de letras
+let text = ' Morango Pessego Uva Abacaxi Laranja ';
+console.log(text.length);
+
+//3 primeiros caracteres
+const str = ' Abacaxi ';
+console.log(str.substring(0,2));
+
+//Limpar caracter em branco
+let text2 = '        Uva';
+console.log(text2.trim());
+
+//Texto em caixa baixa
+const frutas = 'Morango Pessego Uva';
+console.log(frutas.toLowerCase());
+
+//Exibir listagem de frutas
+console.log(' Lista $[Morango] ')
