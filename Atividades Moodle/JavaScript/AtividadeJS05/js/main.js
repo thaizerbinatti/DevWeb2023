fe12d31c31324cd6a15794fc009a5b0ba747fd88
thaizerbinatti <thatazerb@gmail.com>
2023-05-31 16:30:57 -0300

    atividades

--- Atividades Moodle/JavaScript/AtividadeJS05/js/main.js
+++ Atividades Moodle/JavaScript/AtividadeJS05/js/main.js
@@ -2,11 +2,24 @@
 
 let contador = 1;
 while (contador <= 50) {
-    console.log(`Número = ${contador}`);
+    console.log(contador);
     contador++;
 }
 
+
+// Contagem regressiva de 10 até 0 | Do-while
+
+let contador2 = 10;
+do {
+    console.log(contador2);
+    contador2--;
+} while (contador2 >= 0);
+
+
 //Números pares de 0 a 100 | For
 
-for (let numPares = 0; numPares < 100; numPares++);
-console.log(`Números pares: ${numPares}`);
+for (let numPares = 0; numPares <= 100; numPares++) {
+    if (numPares % 2 === 0) {
+        console.log(numPares);
+    }
+}
