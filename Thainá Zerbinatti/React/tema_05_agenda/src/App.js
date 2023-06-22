e5fe9ed446fa3aaceb379f8bf7096521937f9efe
thaizerbinatti <thatazerb@gmail.com>
2023-06-22 20:24:31 -0300

    aula 22 junho

---
+++ Thainá Zerbinatti/React/tema_05_agenda/src/App.js
@@ -0,0 +1,11 @@
+import Header from './components/header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header title="tarefas" />
+    </div>
+  );
+}
+
+export default App;
