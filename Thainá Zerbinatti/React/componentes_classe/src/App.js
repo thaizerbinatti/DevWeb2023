7044c1ec675c64b42832ff177aa3091b2a3a941b
thaizerbinatti <thatazerb@gmail.com>
2023-06-21 21:51:38 -0300

    aula 21 junho

---
+++ Thainá Zerbinatti/React/componentes_classe/src/App.js
@@ -0,0 +1,16 @@
+import Exemplo from './components/Exemplo';
+import { Component } from 'react';
+import './App.css';
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Exemplo />
+      </div>
+    );
+  }
+}
+
+export default App;
