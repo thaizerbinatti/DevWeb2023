37ee5963f23de7aaffb9e98252a4dc2702637f8b
thaizerbinatti <thatazerb@gmail.com>
2023-06-22 21:38:49 -0300

    atividades react

---
+++ Atividades Moodle/React/atividade_react03/src/App.js
@@ -0,0 +1,11 @@
+import BoasVindas from './Components/BoasVindas';
+
+function App() {
+  return (
+    <div className="App">
+      <BoasVindas />
+    </div>
+  );
+}
+
+export default App;
---
+++ Atividades Moodle/React/atividade_react03/src/Components/BoasVindas.jsx
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types'
+
+const Section = (props) => {
+    return (
+            <Section>
+                <h1>Bem vindos ao React</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </Section>
+    )
+}
