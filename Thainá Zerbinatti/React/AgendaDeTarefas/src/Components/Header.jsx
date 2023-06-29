d07fdf78732b17b8b14a1d4cb93d91c6e9f0065a
thaizerbinatti <thatazerb@gmail.com>
2023-06-29 20:26:31 -0300

    aula 29 junho

--- Thainá Zerbinatti/React/AgendaDeTarefas/src/Components/Header.jsx
+++ Thainá Zerbinatti/React/AgendaDeTarefas/src/Components/Header.jsx
@@ -1,14 +1,11 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        alert('Clicou');
-    };
+const Header = ({ title, onAdd }) => {
     return (
         <header className="header">
             <h1>Agenda de {title}</h1>
-            <Button bgColor="green" text="Add" onClick={onClick} />
+            <Button bgColor="green" text="Add" onClick={onAdd} />
         </header>
     );
 };
